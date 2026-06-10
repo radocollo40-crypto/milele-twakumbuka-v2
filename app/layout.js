d268349d40c2c125826da7f0b31c9f72ba0512af
Collins Chacha <radocollo40@gmail.com>
2026-06-10 09:11:38 +0300

    Remove duplicate global navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from "@/components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,10 +23,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <Navbar />
-        {children}
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
