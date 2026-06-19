7bc5c40cba976d0202cc7e411add3dafd313e64e
Collins Chacha <radocollo40@gmail.com>
2026-06-19 10:56:39 +0300

    Add robots file for SEO

---
+++ app/robot.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://milele-twakumbuka-v2.vercel.app/sitemap.xml",
+  };
+}
