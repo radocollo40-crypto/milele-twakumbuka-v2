fd9749de5d30a7030f253ce1764be89f66592869
Collins Chacha <radocollo40@gmail.com>
2026-06-19 11:05:15 +0300

    Add sitemap for SEO

---
+++ app/sitemap.js
@@ -0,0 +1,48 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/memorials",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/categories",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/tragedies",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/funeral-announcements",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/add-loved-one",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/privacy-policy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/terms",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/community-guidelines",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://milele-twakumbuka-v2.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
